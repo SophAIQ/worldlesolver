94b9ac78d63453acb46e5fa2122e9b4ff6354614
John Caruthers <john.d.caruthers@gmail.com>
2025-01-12 13:22:59 -0500

    Update data_setup.py

--- countryshape/data_setup.py
+++ countryshape/data_setup.py
@@ -5,7 +5,7 @@ Image classification data.
 """
 # import libraries
 import os
-from torchvision import transforms
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 # Set constants
@@ -37,5 +37,35 @@ def create_dataloaders(
         Where class_names is a list of the target classes.
         Example usage: 
             train_dataloader, test_dataloader, class_names = \
-                = create_dataloaders(train_dir)
+                = create_dataloaders(train_dir=path/to/train_dir,
+                             test_dir=path/to/test_dir,
+                             transform=some_transform,
+                             batch_size=32,
+                             num_workers=4)
     """
+
+    # Use ImageFolder to create dataset(s)
+    train_data = datasets.ImageFolder(train_dir, transform=transform)
+    test_data = datasets.ImageFolder(test_dir, transform=transform)
+
+    # Get class names
+    class_names = train_data.classes
+
+    # Turn images into data loaders
+    train_dataloader = DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    
+    test_dataloader = DataLoader(
+        test_data,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers
+        pin_memory=True,
+    )
+
+    return train_dataloader, test_dataloader, class_names
