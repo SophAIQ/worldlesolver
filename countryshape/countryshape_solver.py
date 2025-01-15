607ba3cf59bb828dd83d664e601f5a7c729f0b1e
John Caruthers <john.d.caruthers@gmail.com>
2025-01-14 22:27:25 -0500

    Update countryshape_solver.py

--- countryshape/countryshape_solver.py
+++ countryshape/countryshape_solver.py
@@ -1,8 +1,12 @@
 ### import packages ###
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from datetime import date
+from pathlib import Path
 from PIL import Image, ImageOps
+import predict
 
 ### Set constants ###
 # Crop pixels
@@ -11,8 +15,18 @@ RIGHT = 1875 #1873
 TOP = 170
 BOTTOM = 720
 
+### Functions
+def type_in_word(word):
+    for i in word:
+            interactable.send_keys(i)
+            time.sleep(0.2)
+    time.sleep(1)
+    play_button.click()
+    time.sleep(3)
+    return 0
+
 # Directory
-image_path = 'C:\\Users\\johnd\\Documents\\School\\Graduate School\\Computer Science\\VSCode\\countryshape\\screenshots\\'
+image_path = Path(r'C:\\Users\\johnd\\Documents\\School\\Graduate School\\Computer Science\\VSCode\\countryshape\\screenshots\\')
 
 ### Create robot ###
 browser = webdriver.Firefox()
@@ -22,19 +36,28 @@ time.sleep(5)
 
 ### get screenshot ###
 current_day = date.today()
-print(current_day)
-browser.save_screenshot(image_path + 'worldle-'+str(current_day)+'.png')
-browser.close()
+file_name = f'worldle-{str(current_day)}.png'
+browser.save_screenshot(image_path / file_name)
 
 ### crop image ###
-img = Image.open(image_path + 'worldle-'+str(current_day)+'.png')
-img.save(image_path + 'raw_wordle-' + str(current_day) + '.png')
+img = Image.open(image_path / file_name)
+img.save(image_path / file_name)
 cropped_image = img.crop((LEFT, TOP, RIGHT, BOTTOM))
-
 
 ### Convert to black and white image ###
 cropped_image_clean = cropped_image.convert("L")
 
 ### Invert to match trained model ###
 inverted_cropped_image_clean = ImageOps.invert(cropped_image_clean)
-inverted_cropped_image_clean.save(image_path + 'worldle-'+str(current_day)+'.png')
+inverted_cropped_image_clean.save(image_path / file_name)
+print(image_path / file_name)
+
+### Make prediction
+prediction = predict.predict64(image_path / file_name)
+
+### Type in prediction
+play_button = browser.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div[2]/div/div[1]/div/div/div/div/div[2]/form/div/button')
+interactable = browser.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div[2]/div/div[1]/div/div/div/div/div[2]/form/div/div/input')
+type_in_word(prediction)
+time.sleep(5)
+browser.close()
